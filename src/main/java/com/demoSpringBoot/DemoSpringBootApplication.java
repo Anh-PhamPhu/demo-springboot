7c1a523330b5e2fca285577b4127b865da1dcc87
Anh.PhamPhu <anhpham.96.k@gmail.com>
2022-06-20 22:42:49 +0900

    Create Project

---
+++ src/main/java/com/demoSpringBoot/DemoSpringBootApplication.java
@@ -0,0 +1,20 @@
+package com.demoSpringBoot;
+
+import com.demoSpringBoot.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootApplication.class, args);
+	}
+
+}
